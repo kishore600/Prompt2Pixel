07a56e82db3f8a539557f80b6bf1f93c19f5c78d
Kishore Kannan <kishore.kannan@assaabloy.com>
2026-09-07 17:30:26 +0530

    inital commit

---
+++ png.py
@@ -0,0 +1,15 @@
+import struct
+import zlib
+# PNG chunk 
+# +--------+------+---------+------+
+# | Length | Type | Data | CRC |
+# +--------+------+---------+------+
+def write_chunk(f, chunk_type, data):
+    f.write(struct.pack('>I', len(data)))
+    f.write(chunk_type)
+    crc = zlib.crc32(data, crc) & 0xffffffff
+    f.write(struct.pack('>I', crc))
+
+    
+with open("test.bin", "wb") as f:
+    write_chunk(f, b'TEST', b'hello')
